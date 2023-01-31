44d090da438d25a5ff10d69fe540d81ac9691207
zahmed <zohaib0718@gmail.com>
2023-01-30 21:57:14 -0600

    Homepage Edits

--- pages/Chat.js
+++ pages/Chat.js
@@ -36,8 +36,7 @@ const Chat = ({text, setMyText, setIsPending}) => {
 			onChange={(e) => setMyValue(e.target.value)} ></TextField>}
         <Form>
           <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-            <Form.Label>Example textarea</Form.Label>
-            <Form.Control as="textarea" rows={3} />
+            <Form.Control as="textarea" rows={6} />
           </Form.Group>
         </Form>
             <Button id = 'submit' variant = 'secondary' onClick = {()=>generate(myValue)}> Submit </Button>
