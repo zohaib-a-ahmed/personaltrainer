400bc0594850ce18c1f6fb2e411546cb54cf9ec9
zahmed <zohaib0718@gmail.com>
2023-02-05 16:43:08 -0600

    About page

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,46 @@
+import { Button, Container, Row } from "react-bootstrap";
+
 const about = () => {
     return ( 
-        <h1>About how this works</h1>
+        <Container>
+            <div className="about">
+                <Row>
+                    <h2> How it Works </h2>
+                </Row>
+                <Row>
+                    <p>
+                        <b>Atlas</b> is a machine-learning personal fitness trainer powered by the state-of-the-art GPT-3 technology developed by OpenAI. 
+                        Our goal is to provide you with the most comprehensive and personalized fitness experience possible. 
+                        With expertise in nutrition, bodybuilding, and weightlifting, Atlas is dedicated to helping people achieve their fitness goals.
+                        Start your adventure today!
+                    </p>
+                </Row>
+                <Row>
+                    <p>
+                        In order to use <b>Atlas</b>, you will need an API key from OpenAI. If you don't already have an account,
+                        you will need to create one. 
+                    </p>
+                    <img src="login.png" alt="OpenAI Login" className="center-image" style={{width: "700px"}}/>
+                </Row>
+                <Row>
+                    <p>
+                        Once you've created an account, or if you already have one, navigate to the top left corner of the page and select your account.
+                    </p>
+                    <img src="homepage.png" alt="OpenAI Login" className="center-image" style={{width: "700px"}}/>
+                </Row>
+                <Row>
+                    <p>
+                        From there, select "View API keys", and come to this page. Create a new secret key and copy it to use Atlas.
+                    </p>
+                    <img src="apikey.png" alt="OpenAI Login" className="center-image" style={{width: "700px"}}/>
+                </Row>
+                <Row>
+                    <div className="buttons">
+                        <Button size = 'lg' variant="dark" href="/chat">Ready?</Button>
+                    </div>
+                </Row>
+            </div>
+        </Container>
      );
 }
  
