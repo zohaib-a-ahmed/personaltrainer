28c631e848be8c77c76caa4bf804fc73ccb07cab
zahmed <zohaib0718@gmail.com>
2023-02-05 23:36:40 -0600

    build bugs

--- pages/about.js
+++ pages/about.js
@@ -17,20 +17,20 @@ const about = () => {
                 </Row>
                 <Row>
                     <p>
-                        In order to use <b>Atlas</b>, you will need an API key from OpenAI. If you don't already have an account,
-                        you will need to create one <a href="https://beta.openai.com/signup" target="_blank">here</a>.
+                        In order to use <b>Atlas</b>, you will need an API key from OpenAI. If you don&apos;t already have an account,
+                        you will need to create one <a href="https://beta.openai.com/signup" target="_blank" rel="noreferrer">here</a>.
                     </p>
                     <img src="login.png" alt="OpenAI Login" className="center-image" style={{width: "700px"}}/>
                 </Row>
                 <Row>
                     <p>
-                        Once you've created an account, or if you already have one, navigate to the top left corner of the page and select your account.
+                        Once you&apos;ve created an account, or if you already have one, navigate to the top left corner of the page and select your account.
                     </p>
                     <img src="homepage.png" alt="OpenAI Login" className="center-image" style={{width: "700px"}}/>
                 </Row>
                 <Row>
                     <p>
-                        From there, select "View API keys", and come to this page. Create a new secret key and copy it to use Atlas.
+                        From there, select &quot;View API keys&quot;, and come to this page. Create a new secret key and copy it to use Atlas.
                     </p>
                     <img src="apikey.png" alt="OpenAI Login" className="center-image" style={{width: "700px"}}/>
                 </Row>
